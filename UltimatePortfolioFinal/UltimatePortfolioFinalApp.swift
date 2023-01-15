a27b19011378761339fd51b0a96af29fa5c533ff
milanmach2 <milanmach2@gmail.com>
2023-01-15 11:41:08 +0100

    Initial Commit

---
+++ UltimatePortfolioFinal/UltimatePortfolioFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioFinalApp.swift
+//  UltimatePortfolioFinal
+//
+//  Created by Milan Mach on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
