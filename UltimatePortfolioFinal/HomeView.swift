0f9b16ecc759ba8749490b9036dde87a9b532986
milanmach2 <milanmach2@gmail.com>
2023-01-15 13:42:10 +0100

    AppStorage

---
+++ UltimatePortfolioFinal/HomeView.swift
@@ -0,0 +1,32 @@
+//
+//  HomeView.swift
+//  UltimatePortfolioFinal
+//
+//  Created by Milan Mach on 15.01.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    static let tag: String? = "Home"
+    @EnvironmentObject var dataController: DataController
+    var body: some View {
+           NavigationView {
+               VStack {
+                   Button("Add Data") {
+                       dataController.deleteAll()
+                       try? dataController.createSampleData()
+                   }
+               }
+               .navigationTitle("HomeFinal")
+           }
+       }
+   }
+
+
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
--- UltimatePortfolioFinal/UltimatePortfolioFinalApp.swift
+++ UltimatePortfolioFinal/UltimatePortfolioFinalApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct UltimatePortfolioFinalApp: App {
+    @StateObject var dataController: DataController
+    init() {
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)// for SwiftUI
+                    .environmentObject(dataController)// our own code
+
         }
     }
 }
